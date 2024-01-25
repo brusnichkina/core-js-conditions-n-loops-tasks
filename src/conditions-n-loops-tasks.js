29a2074fcd0623c81cb0f7fe9920af7695bd87c6
brusnichkina <bru_snichkina@icloud.com>
2024-01-25 20:22:01 +0300

    feat: complete some tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -299,8 +299,21 @@ function isContainNumber(num, digit) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  for (let i = 1; i < arr.length; i += 1) {
+    let sumLeft = 0;
+    let sumRight = 0;
+    for (let l = 0; l < i; l += 1) {
+      sumLeft += arr[l];
+    }
+    for (let r = i + 1; r < arr.length; r += 1) {
+      sumRight += arr[r];
+    }
+    if (sumLeft === sumRight) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
